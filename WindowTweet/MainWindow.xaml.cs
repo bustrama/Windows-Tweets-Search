fb7742bc2af0c8c9bd5319ce54f2d6c03d634a30
rubik951 <rubik.junk@gmail.com>
2014-03-17 16:58:29 +0200

    Everything Works, added Background

--- WindowTweet/MainWindow.xaml.cs
+++ WindowTweet/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Net;
 using System.Web;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace WindowTweet
 {
@@ -35,35 +36,52 @@ namespace WindowTweet
             return System.Convert.ToBase64String(textBytes);
         }
 
-        public void auth()
+        public static string auth()
         {
-            WebClient wc = new WebClient();
-            wc.Headers["Host"] = "api.twitter.com";
-            //wc.Headers["Content-Type"] = "application/x-www-form-urlencoded;charset=UTF-8";
-            //wc.Headers["Content-Length"] = "29";
-            wc.Headers["User-Agent"] = "Windows Tweets Search v1.0";
-            wc.Headers["Authorization"] = "Basic " + Base64Encode("F2Ll18JPHROTclBPabbQ:T55syYJQlFYzbDLc2LjKdr3TQJNalvYv9OSINWIBk0");
+            WebClient wcAuth = new WebClient();
+            wcAuth.Headers["Host"] = "api.twitter.com";
+            wcAuth.Headers["User-Agent"] = "Windows Search Tweets";
+            wcAuth.Headers["Authorization"] = "Basic " + Base64Encode("F2Ll18JPHROTclBPabbQ:T55syYJQlFYzbDLc2LjKdr3TQJNalvYv9OSINWIBk0");
 
             NameValueCollection auth = new NameValueCollection();
             auth["grant_type"] = "client_credentials";
-            byte[] response = wc.UploadValues("https://api.twitter.com/oauth2/token", "POST", auth);
-            var str = System.Text.Encoding.Default.GetString(response);
-            result.Text = str;
+
+            string accessToken = System.Text.Encoding.Default.GetString(wcAuth.UploadValues("https://api.twitter.com/oauth2/token", "POST", auth));
+            accessToken = (new JavaScriptSerializer().DeserializeObject(accessToken) as Dictionary<string, object>)["access_token"].ToString();
+            return accessToken;
         }
 
-        public static void search(string request)
+        public static IEnumerable<Tweet> search(string word)
         {
-
+            WebClient wc = new WebClient();
+            wc.Headers["Authorization"] = "Bearer " + auth();
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            string tweets = wc.DownloadString("https://api.twitter.com/1.1/search/tweets.json?q=" + word);
+            object[] tweetslist = (jss.DeserializeObject(tweets) as Dictionary<string, object>)["statuses"] as object[];
+            foreach (var item in tweetslist)
+            {
+                // inside this loop, tweet is json["statuses"][index]
+                var tweet = item as Dictionary<string, object>;
+                yield return new Tweet() { userTweet = tweet["text"].ToString(), userImage = (tweet["user"] as Dictionary<string, object>)["profile_image_url"].ToString() };
+            }
         }
 
         private void refreshMe(object sender, RoutedEventArgs e)
         {
-            auth();
+            string word = searchEngine.Text;
+            tweetsBox.DataContext = search(word);
         }
 
         private void search(object sender, RoutedEventArgs e)
         {
-
+            string word = searchEngine.Text;
+            tweetsBox.DataContext = search(word);
         }
     }
+}
+
+public class Tweet
+{
+    public string userTweet { get; set; }
+    public string userImage { get; set; }
 }
--- WindowTweet/Properties/Resources.Designer.cs
+++ WindowTweet/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace WindowTweet.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap TwitterApplicationBackground {
+            get {
+                object obj = ResourceManager.GetObject("TwitterApplicationBackground", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
